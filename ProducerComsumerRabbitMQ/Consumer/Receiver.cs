8d12c4fc89c44ad707e2a766d453f36537e62125
` <huy.dang@orientsoftware.com>
2019-08-17 17:59:39 +0700

    Basic control C# RabbitMQ

---
+++ ProducerComsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("DangTuanHuy", false, false, false, null);
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+
+                    var ms = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine("Recevice Mess {0}", ms);
+
+                };
+                channel.BasicConsume("DangTuanHuy", true, consumer);
+
+                Console.WriteLine("Press Enter to Exit");
+                Console.ReadLine();
+            }
+            
+        }
+    }
+}
---
+++ ProducerComsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    public class Sender
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("DangTuanHuy", false, false, false, null);
+
+                string message = "Getting started with .Net Core RebbitMQ";
+
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "DangTuanHuy", null, body);
+                Console.WriteLine("Sent Message {0}....", message);
+
+
+            }
+            Console.WriteLine("Press [enter] to exits the Sender App");
+            Console.ReadLine();
+
+        }
+    }
+}
