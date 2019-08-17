05e3b59066a96521cc6c455d34d857a6ef20fe18
` <huy.dang@orientsoftware.com>
2019-08-17 18:40:37 +0700

    Update MS

---
+++ MicroserviceRabbitMQ/MicoRabbit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,20 @@
+﻿using MicoRabbit.Domain.Core.Commands;
+using MicoRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MicoRabbit.Domain.Core.Bus
+{
+    public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Publish<T>(T @event) where T : Event;
+
+        void Subsribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+
+    }
+}
---
+++ MicroserviceRabbitMQ/MicoRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,19 @@
+﻿using MicoRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicoRabbit.Domain.Core.Commands
+{
+    public abstract class Command : Message
+    {
+        public DateTime Timestamp { get; protected set; }
+
+        protected Command()
+        {
+            Timestamp = DateTime.UtcNow;
+
+        }
+    }
+    
+}
